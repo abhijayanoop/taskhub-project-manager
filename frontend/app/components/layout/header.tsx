fea999b21160550d54acd6159db5d33111cb0563
abhijayanoop <abhijayanoop007@gmail.com>
2025-10-01 10:57:39 +0530

    dashboard ui layout

---
+++ frontend/app/components/layout/header.tsx
@@ -0,0 +1,116 @@
+import { useAuth } from "@/provider/auth-context";
+import type { Workspace } from "@/types";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { Bell, PlusCircle } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Link } from "react-router";
+import { WorkspaceAvatar } from "../workspace/workspace-avatar";
+
+interface HeaderProps {
+  onWorkspaceSelected: (workspace: Workspace) => void;
+  selectedWorkspace: Workspace | null;
+  onCreateWorkspace: () => void;
+}
+
+const Header = ({
+  onWorkspaceSelected,
+  selectedWorkspace,
+  onCreateWorkspace,
+}: HeaderProps) => {
+  const { user } = useAuth();
+
+  const workspaces = [];
+
+  return (
+    <div className="bg-background sticky top-0 z-40 border-b">
+      <div className="flex h-14 items-center justify-between px-4 sm:px-6 lg:px-8 py-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"outline"}>
+              {selectedWorkspace ? (
+                <>
+                  {selectedWorkspace.color && (
+                    <WorkspaceAvatar
+                      color={selectedWorkspace.color}
+                      name={selectedWorkspace.name}
+                    />
+                  )}
+                  <span className="font-medium">{selectedWorkspace?.name}</span>
+                </>
+              ) : (
+                <span className="font-medium">Select Workspace</span>
+              )}
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Workspace</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuGroup>
+              {/* {workspaces.map((ws) => (
+                <DropdownMenuItem
+                  key={ws._id}
+                  onClick={() => handleOnClick(ws)}
+                >
+                  {ws.color && (
+                    <WorkspaceAvatar color={ws.color} name={ws.name} />
+                  )}
+                  <span className="ml-2">{ws.name}</span>
+                </DropdownMenuItem>
+              ))} */}
+            </DropdownMenuGroup>
+
+            <DropdownMenuGroup>
+              <DropdownMenuItem onClick={onCreateWorkspace}>
+                <PlusCircle className="w-4 h-4 mr-2" />
+                Create Workspace
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <div className="flex items-center gap-2">
+          <Button variant="ghost" size="icon">
+            <Bell />
+          </Button>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <button className="rounded-full border p-1 w-8 h-8">
+                <Avatar className="w-8 h-8">
+                  <AvatarImage src={user?.profilePicture} alt={user?.name} />
+                  <AvatarFallback className="bg-primary text-primary-foreground">
+                    {user?.name?.charAt(0).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              </button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link to="/user/profile">Profile</Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => {}}>Log Out</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/app/routes/dashboard/dashboard-layout.tsx
+++ frontend/app/routes/dashboard/dashboard-layout.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import Header from "@/components/layout/header";
+import SidebarComponent from "@/components/layout/sidebar-component";
+import { CreateWorkspace } from "@/components/workspace/create-workspace";
+import { useAuth } from "@/provider/auth-context";
+import type { Workspace } from "@/types";
+import { Loader } from "lucide-react";
+import React, { useState } from "react";
+import { Navigate, Outlet } from "react-router";
 
 const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+  const { user, isAuthenticated, isLoading } = useAuth();
+
+  const [isCreatingWorkspace, setIsCreatingWorkspace] = useState(false);
+  const [currentWorkspace, setCurrentWorkspace] = useState<Workspace | null>(
+    null
+  );
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  const handleWorkspaceSelected = (workspace: Workspace) => {
+    setCurrentWorkspace(workspace);
+  };
+
+  return (
+    <div className="flex h-screen w-full">
+      <SidebarComponent currentWorkspace={currentWorkspace} />
+
+      <div className="flex flex-1 flex-col h-full">
+        <Header
+          onWorkspaceSelected={handleWorkspaceSelected}
+          selectedWorkspace={currentWorkspace}
+          onCreateWorkspace={() => setIsCreatingWorkspace(true)}
+        />
+
+        <main className="flex-1 overflow-y-auto h-full w-full">
+          <div className="mx-auto container px-2 sm:px-6 lg:px-8 py-0 md:py-8 w-full h-full">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+
+      <CreateWorkspace
+        isCreatingWorkspace={isCreatingWorkspace}
+        setIsCreatingWorkspace={setIsCreatingWorkspace}
+      />
+    </div>
+  );
 };
 
 export default DashboardLayout;
